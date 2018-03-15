b7e338e60213e3e45baa01fa3413de20915db78e
pgorovoy <pgorovoy@playflock.com>
2018-03-15 18:10:08 +0300

    push!

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import './components/index.less';
+import './components/App.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import Store from './Store';
