87e9bbbe1a1b517a3e320cf0860c69ba7eda29e3
pgorovoy <pgorovoy@playflock.com>
2018-03-15 13:29:16 +0300

    update

--- src/components/content/Cards.jsx
+++ src/components/content/Cards.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Button, Icon } from 'antd';
 
 const gridStyle = {
     width: '20%',
@@ -8,9 +8,25 @@ const gridStyle = {
 
 export default class Cards extends React.Component {
 
+    handleEditClick = () => {
+
+    }
+
+    handleDeleteClick = () => {
+
+    }
+
     render() {
         return (<Card className="userCard" title={this.props.name} style={gridStyle}>
                 {this.props.email}
+                <div className="userActions">
+                    <Button className="user-button" type="primary" onClick={this.handleEditClick}>
+                        <Icon type="edit" /> Edit
+                    </Button>
+                    <Button className="user-button" type="danger" onClick={this.handleDeleteClick}>
+                        <Icon type="delete" /> Delete
+                    </Button>
+                </div>
             </Card>
         )
     }
